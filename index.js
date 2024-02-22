61a7865676fc6ddd3ae23cd0dda69934754ac4c0
robbietonos37 <109938015+robbietonos37@users.noreply.github.com>
2024-02-21 20:29:42 -0600

    Add daily high and low prices

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-const stockSecret = getKey();
+// const express = require('express');
+// require('dotenv').config()
+// const app = express();
+// const port = 4000;  // defaults to 4000
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// })
+
+// app.listen(port, () => {
+//     console.log(`Server running on http://localhost:${port}/`);
+// });
+
+// const stockSecret = process.env.STOCK_SECRET;
+const stockSecret = getStockSecret();
 
 const stockSearcherForm = document.querySelector('#stock-search-form');
 const stockTickerInput = document.querySelector('#stock-ticker-input');
 const companyNameDisplay = document.querySelector('#company-name');
 const stockDataPrice = document.querySelector('#stock-price');
+const dayHighPrice = document.querySelector('#day-high');
+const dayLowPrice = document.querySelector('#day-low');
 
 
 stockSearcherForm.addEventListener('submit', (e) => {
@@ -16,6 +32,8 @@ stockSearcherForm.addEventListener('submit', (e) => {
             const stockDataFinal = await stockDataResponse.json();
             companyNameDisplay.textContent = stockDataFinal.data[0].name;
             stockDataPrice.textContent = stockDataFinal.data[0].price;
+            dayHighPrice.textContent = 'Daily High: ' + stockDataFinal.data[0].day_high;
+            dayLowPrice.textContent = 'Daily Low: ' + stockDataFinal.data[0].day_low;
 
         } catch (err) {
             console.log('Error: ' + err + ' for request')
